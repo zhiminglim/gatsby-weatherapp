4b5705e86596caf35c4ad29c9b3a9af3773133f0
Zhiming <i-jovial@hotmail.com>
2021-02-02 00:01:42 +0800

    Add Google Maps in second page

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,16 +1,36 @@
-import React from "react"
+import React, { useState } from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import GoogleMapReact from "google-map-react"
 
-const SecondPage = () => (
-  <Layout>
-    <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
-)
+function SecondPage() {
+
+  const [mapCenter, setMapCenter] = useState({
+    lat: 1.3521,
+    lng: 103.8198
+  })
+  const [zoom, setZoom] = useState(11);
+
+
+
+  return (
+    <Layout>
+      <SEO title="Page two" />
+      <div style={{ width: "100%", height: "600px" }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: process.env.GOOGLE_MAP_API }}
+          defaultCenter={mapCenter}
+          defaultZoom={zoom}
+        >
+
+        </GoogleMapReact>
+      </div>
+
+      <Link to="/">Go back to the homepage</Link>
+    </Layout>
+  );
+}
 
 export default SecondPage
